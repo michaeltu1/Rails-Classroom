00631374362d17b1658e15ce36883f06a0d604d8
Yudi Tan <yudi98@berkeley.edu>
2017-11-24 17:46:35 -0600

    tweaked paperclip settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,15 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV.fetch('S3_BUCKET_NAME'),
-    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('AWS_REGION'),
-  }
-}
+    storage: :s3,
+    s3_region: ENV["AWS_S3_REGION"],
+    s3_credentials: {
+      s3_host_name: ENV["AWS_S3_HOST_NAME"],
+      bucket: ENV["AWS_S3_BUCKET"],
+      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+      }
+    } 
 
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
