81417f09c44b2be74cf701b60964d345b42ba05c
Yudi Tan <yudi98@berkeley.edu>
2017-11-24 18:28:09 -0600

    stil tweaking

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,7 @@ Rails.application.configure do
     s3_region: ENV["AWS_REGION"],
     s3_credentials: {
       bucket: ENV["S3_BUCKET_NAME"],
+      :preserve_files => true,
       access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
       }
